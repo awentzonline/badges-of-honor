6541c72b2f027887ba0c0641b19e099fa0090290
Adam Wentz <adam@adamwentz.com>
2015-07-03 11:17:57 -0500

    buncha crap

--- js/game.js
+++ js/game.js
@@ -233,6 +233,8 @@ GameOver.prototype = {
     }
     this.instructionText = this.game.add.text(this.game.world.centerX, 300, 'Click To Play Again', { font: '16px Arial', fill: '#ffffff', align: 'center'});
     this.instructionText.anchor.setTo(0.5, 0.5);
+
+    this.game.score = 0;
   },
   update: function () {
     if(this.game.input.activePointer.justPressed()) {
@@ -306,6 +308,75 @@ module.exports = [
       },
       {
         x: 0.6,
+        y: 0.78
+      }
+    ]
+  },
+  {
+    background: 'level1.jpg',
+    targets: [
+      {
+        x: 0.6,
+        y: 0.8
+      },
+      {
+        x: 0.55,
+        y: 0.85
+      },
+      {
+        x: 0.61,
+        y: 0.75
+      },
+      {
+        x: 0.6,
+        y: 0.78
+      },
+      {
+        x: 0.2,
+        y: 0.8
+      },
+      {
+        x: 0.25,
+        y: 0.85
+      },
+      {
+        x: 0.21,
+        y: 0.75
+      },
+      {
+        x: 0.2,
+        y: 0.78
+      },
+      {
+        x: 0.4,
+        y: 0.8
+      },
+      {
+        x: 0.35,
+        y: 0.85
+      },
+      {
+        x: 0.41,
+        y: 0.75
+      },
+      {
+        x: 0.4,
+        y: 0.78
+      },
+      {
+        x: 0.8,
+        y: 0.8
+      },
+      {
+        x: 0.78,
+        y: 0.85
+      },
+      {
+        x: 0.81,
+        y: 0.75
+      },
+      {
+        x: 0.8,
         y: 0.78
       }
     ]
@@ -473,8 +544,8 @@ Play.prototype = {
         new ReaderAction({
           textObject: this.commandText,
           randomLines: [
-            ['Good Job. Level Complete.'],
-            ['Great shooting, soldier.\nGet ready for the next level.']
+            ['Good Job\nLevel Complete'],
+            ['Great shooting, soldier\nGet ready for the next level']
           ]
         }),
         {
