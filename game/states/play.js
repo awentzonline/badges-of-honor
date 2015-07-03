950b74d998d4c1c5165b18c9931676387224645d
Adam Wentz <adam@adamwentz.com>
2015-07-02 21:37:23 -0500

    blood blood blood blood blood blood blood blood blood

--- game/states/play.js
+++ game/states/play.js
@@ -22,7 +22,13 @@ Play.prototype = {
   },
   create: function() {
     this.background = this.game.add.sprite(0, 0, 'background');
-    
+    // splatter
+    this.bloodEmitter = this.game.add.emitter(0, 0, 100);
+    this.bloodEmitter.makeParticles('bloodsplat');
+    this.bloodEmitter.gravity = 600;
+    this.bloodEmitter.setAlpha(0.5, 0, 1000);
+    this.bloodEmitter.setRotation(0, 0);
+    // enemies
     this.enemyGroup = this.game.add.group();
     _.each(this.levelConfig.targets, function (conf) {
       var enemy = new Enemy(this.game, this.game.width * conf.x, this.game.height * conf.y)
@@ -146,6 +152,7 @@ Play.prototype = {
       }
       if (hitEnemy) {
         this.enemyShot(hitEnemy);
+        this.bloodBurst(pointer.x, pointer.y);
       }
       // if (!this.allowedToShoot) {
       //   this.onLose('You were executed for shooting too early.');
@@ -189,6 +196,11 @@ Play.prototype = {
     tween.onComplete.add(function () {
       blip.kill();
     });
+  },
+  bloodBurst: function (x, y) {
+    this.bloodEmitter.x = x;
+    this.bloodEmitter.y = y;
+    this.bloodEmitter.start(true, 500, null, 3);
   },
   onWin: function () {
     this.winTriggered = true;
--- game/states/preload.js
+++ game/states/preload.js
@@ -13,6 +13,7 @@ Preload.prototype = {
     this.load.setPreloadSprite(this.asset);
     this.load.spritesheet('soldier', 'assets/soldier_fall.png', 61, 200, 15);
     this.load.image('crosshair', 'assets/crosshair.png');
+    this.load.image('bloodsplat', 'assets/bloodsplat.png');
     this.load.audio('shoot', 'assets/ar15.wav');
     this.load.audio('hit', 'assets/hit.wav');
   },
